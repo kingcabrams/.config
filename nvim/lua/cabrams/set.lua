a92be3291ddfbaf089b4050bfb97cc1cc8b14709
Caleb Abrams <cabrams@mit.edu>
2024-04-29 09:52:17 -0400

    adds alacritty config

--- nvim/lua/cabrams/set.lua
+++ nvim/lua/cabrams/set.lua
@@ -6,7 +6,7 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.expandtab = false
 
 vim.opt.smartindent = true
 
@@ -20,3 +20,25 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
+
+vim.opt.conceallevel = 2
+vim.opt.list = true
+vim.opt.listchars = {
+  tab = '» ',
+  leadmultispace = '· ',
+  trail = '␣',
+  extends = '▶',
+  precedes = '◀',
+  nbsp = '␣',
+}
+
+vim.opt.foldmethod = 'syntax'
+vim.opt.syntax = 'on'
+
+vim.opt.cindent = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.opt.foldlevel = 1
